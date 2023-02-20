9889a5e1f723783d53da7caf0a83f96459e8ea79
DavideDiFranco95 <Davide.difranco_@outlook.it>
2023-02-20 12:28:55 +0100

    Aggiornamento Switch e modifiche

--- src/Contracts.java
+++ src/Contracts.java
@@ -11,4 +11,5 @@ public enum Contracts {
         SOMMINISTRAZIONE
     }
 
+
     //creare metodo per switchare contratto
--- src/ContrattoNazionaleDB.java
+++ src/ContrattoNazionaleDB.java
@@ -13,4 +13,48 @@ public class ContrattoNazionaleDB {
         this.internshipAlreadyDone = internshipAlreadyDone;
         this.levelOfContract = levelOfContract;
     }
+
+    /**
+     * Added void method printContracts for switch
+     * @param contract of the single employee
+     */
+    public static void printContracts(Contracts contract){
+        switch(contract){
+            case DETERMINATO:
+                System.out.println("Contratto a tempo determinato");
+                break;
+            case INDETERMINATO:
+                System.out.println("Contratto a tempo indeterminato");
+                break;
+            case PART_TIME:
+                System.out.println("Contratto Part Time");
+                break;
+            case APPRENDISTATO:
+                System.out.println("Contratto di apprendistato");
+                break;
+            case STAGE:
+                System.out.println("Contratto di Stage");
+                break;
+            case INTERMITTENTE:
+                System.out.println("Contratto Intermittente");
+                break;
+            case PRESTAZIONI_OCCASIONALI:
+                System.out.println("Contratto a Prestazioni Occasionali");
+                break;
+            case AUSILIARIO:
+                System.out.println("Contratto di tipo Ausiliario");
+                break;
+            case CO_CO_CO:
+                System.out.println("Contratto Co Co Co");
+                break;
+            case SOMMINISTRAZIONE:
+                System.out.println("Contratto a Somministrazione");
+                break;
+        }
+    }
+    public Contracts getTypeOfContract() {
+        return typeOfContract;
+
+    }
+
 }
